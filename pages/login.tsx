4ac768064b3b2cb75ef393f31979fabec5fbf9f1
Sheikh Taha Jameel <87434787+TahiJameel@users.noreply.github.com>
2022-01-19 13:09:17 +0500

    Fix title issue

--- pages/login.tsx
+++ pages/login.tsx
@@ -4,16 +4,20 @@ import { auth } from '../firebase';
 import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
 import useAuth from '../hooks/useAuth'
 import { useRouter } from 'next/router'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 const Login: NextPage = () => {
   const provider = new GoogleAuthProvider();
   const { user, setUser } = useAuth();
   const router = useRouter();
+  const [title, setTitle] = useState<string>('Login');
 
   useEffect(() => {
     if (user) {
-      router.push('/')
+      router.replace('/')
+      setTitle('Home')
+    } else {
+      setTitle('Login')
     }
   }, [user, router])
 
@@ -31,23 +35,16 @@ const Login: NextPage = () => {
   return (
     <div>
       <Head>
-        <title>{user ? 'Loading...' : 'Login'}</title>
+        <title>{title}</title>
       </Head>
-      {user ? (
-        <div className='flex justify-center items-center h-screen flex-col'>
-          <h1>Loading...</h1>
-        </div>
-      ) : (
-        <div className='flex justify-center items-center h-screen flex-col'>
-          <h1 className='text-3xl font-semibold'>Login</h1>
-          <button onClick={loginUser} className='bg-teal-500 text-white px-4 py-2 rounded duration-1000 hover:bg-white hover:text-teal-500 mt-2'>
-            Login with Google
-          </button>
-        </div>
-      )}
+      <div className='flex justify-center items-center h-screen flex-col'>
+        <h1 className='text-3xl font-semibold'>Login</h1>
+        <button onClick={loginUser} className='bg-teal-500 text-white px-4 py-2 rounded duration-1000 hover:bg-white hover:text-teal-500 mt-2'>
+          Login with Google
+        </button>
+      </div>
     </div>
   )
 }
 
 export default Login;
-
