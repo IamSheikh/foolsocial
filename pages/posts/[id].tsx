9bbc8546903db2787f8729c317d92cf564452b9c
Sheikh Taha Jameel <87434787+TahiJameel@users.noreply.github.com>
2022-01-23 22:53:48 +0500

    change fallback to true

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,26 +1,26 @@
-import type { GetStaticProps, NextPage } from 'next'
-import Head from 'next/head'
-import AddCommentForm from '../../components/AddCommentForm'
-import Comment from '../../components/Comment'
-import dbConnect from '../../lib/mongo'
-import Post from '../../models/Post'
-import Image from 'next/image'
-import useAuth from '../../hooks/useAuth'
-import { useEffect, useState } from 'react'
+import type { GetStaticProps, NextPage } from "next";
+import Head from "next/head";
+import AddCommentForm from "../../components/AddCommentForm";
+import Comment from "../../components/Comment";
+import dbConnect from "../../lib/mongo";
+import Post from "../../models/Post";
+import Image from "next/image";
+import useAuth from "../../hooks/useAuth";
+import { useEffect, useState } from "react";
 
 interface Props {
   post: {
-    _id: string
-    name: string
-    body: string
-    img: string
-  }
+    _id: string;
+    name: string;
+    body: string;
+    img: string;
+  };
 }
 
 const PostById: NextPage<Props> = (props: Props) => {
-  const { user } = useAuth()
-  const [message, setMessage] = useState('')
-  const [comments, setComments] = useState([])
+  const { user } = useAuth();
+  const [message, setMessage] = useState("");
+  const [comments, setComments] = useState([]);
   const addComment = (comment: string, setComment: Function) => {
     if (user) {
       const userComment = {
@@ -28,93 +28,93 @@ const PostById: NextPage<Props> = (props: Props) => {
         postId: props.post._id,
         img: user.photo,
         comment: comment,
-      }
-      fetch('/api/comment/add-comment', {
-        method: 'POST',
+      };
+      fetch("/api/comment/add-comment", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(userComment),
       })
         .then(async () => {
-          setComment('')
-          setMessage('Comment Posted! Refresh to see the comment')
+          setComment("");
+          setMessage("Comment Posted! Refresh to see the comment");
 
           setTimeout(() => {
-            setMessage('')
-          }, 10000)
+            setMessage("");
+          }, 10000);
         })
         .catch((err) => {
-          console.log(err)
-        })
+          console.log(err);
+        });
     }
-  }
+  };
   useEffect(() => {
     fetch(`/api/comment/${props.post._id}`)
       .then(async (res) => await res.json())
-      .then((data) => setComments(data))
-  }, [])
+      .then((data) => setComments(data));
+  }, []);
   return (
     <div>
       <Head>
         <title>Post</title>
       </Head>
-      <div className='ml-2 mt-5'>
-        <h1 className='text-3xl font-semibold'>{props.post.name}</h1>
+      <div className="ml-2 mt-5">
+        <h1 className="text-3xl font-semibold">{props.post.name}</h1>
         <p>{props.post.body}</p>
-        <div className='flex justify-center'>
-          {props.post.img !== '' && (
+        <div className="flex justify-center">
+          {props.post.img !== "" && (
             <>
               <Image
                 src={props.post.img}
                 width={500}
                 height={500}
-                className='text-center'
+                className="text-center"
               />
             </>
           )}
         </div>
-        <div className='flex items-center flex-col mt-10'>
-          <h1 className='text-3xl font-semibold'>Comments</h1>
-          {message !== '' && <h3>{message}</h3>}
+        <div className="flex items-center flex-col mt-10">
+          <h1 className="text-3xl font-semibold">Comments</h1>
+          {message !== "" && <h3>{message}</h3>}
           <AddCommentForm handler={addComment} />
           {comments instanceof Array &&
             comments.map(
               (comment: {
-                name: string
-                postId: string
-                img: string
-                comment: string
-                _id: string
+                name: string;
+                postId: string;
+                img: string;
+                comment: string;
+                _id: string;
               }) => <Comment key={comment._id} comment={comment} />
             )}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export const getStaticPaths = async () => {
-  await dbConnect()
+  await dbConnect();
 
-  const result = await Post.find({})
+  const result = await Post.find({});
 
   const posts = result.map((doc: any) => {
-    const post = doc.toObject()
-    post._id = post._id.toString()
-    return { params: { id: post._id } }
-  })
+    const post = doc.toObject();
+    post._id = post._id.toString();
+    return { params: { id: post._id } };
+  });
 
   return {
     paths: posts,
-    fallback: false,
-  }
-}
+    fallback: true,
+  };
+};
 
 export const getStaticProps: GetStaticProps = async (context) => {
-  await dbConnect()
+  await dbConnect();
 
-  const post = await Post.findById(context.params?.id).lean()
+  const post = await Post.findById(context.params?.id).lean();
 
   return {
     props: {
@@ -126,7 +126,7 @@ export const getStaticProps: GetStaticProps = async (context) => {
       },
     },
     revalidate: 10,
-  }
-}
+  };
+};
 
-export default PostById
+export default PostById;
